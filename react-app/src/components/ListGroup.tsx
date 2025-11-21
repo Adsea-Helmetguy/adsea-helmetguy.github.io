c9eb4b68ca5eb4ef930b39bd83c8cb3253b422a8
mlow <marcuslowsj@gamil.com>
2025-11-21 20:35:40 +0800

    contiune ltr

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";//Replacement of div
+
+function ListGroup() {
+  return (
+	//<h1>A component cannot return more than one element</h1>
+	<Fragment>
+		<h1>List</h1>
+			<ul className="list-group">
+			  <li className="list-group-item">An item</li>
+			  <li className="list-group-item">A second item</li>
+			  <li className="list-group-item">A third item</li>
+			  <li className="list-group-item">A fourth item</li>
+			  <li className="list-group-item">And a fifth one</li>
+			</ul>
+	</Fragment>
+  );
+}
+
+export default ListGroup;
