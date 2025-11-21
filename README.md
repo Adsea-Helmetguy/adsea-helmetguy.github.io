baa6dbb999d53dc7b391822648653c354a3a3840
mlow <marcuslowsj@gamil.com>
2025-11-21 19:13:21 +0800

    Trying with just react and typescript for now

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # adsea-helmetguy.github.io
 
 Currently still in development, will try to get a working About me page asap.
-
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+
+
+// PascalCasing, capitalise the first letter of everyword.
+export function Message () {
+
+    // JSX: JavaScript XML
+    return (<h1>Hello World</h1>);
+}
+
+export default Message;
